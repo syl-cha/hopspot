f83ad5dea6153338013cb82070b3d96b1fa4fff7
MaxLEG <soldier322@gmail.com>
2025-10-31 23:48:13 +0100

    map and display of brawery added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,17 @@ import {
   getAll,
   getFiltered,
   getMetadata,
-} from './services/openBreweryService.js';
+} from "./services/openBreweryService.js";
 import {
   getRandomBrewery,
   getAmericanStates,
-} from './utils/openBreweryUtils.js';
-import { BreweryCardBuilder } from './builders/builders.js';
+} from "./utils/openBreweryUtils.js";
+import { BreweryCardBuilder } from "./builders/builders.js";
+import { initializeMap, setBreweryMarker } from "./services/map.js";
 $(document).ready(async function () {
-  const $breweryDiv = $('#random-brewery');
+  const $breweryDiv = $("#random-brewery");
   $breweryDiv.text("Récupération d'une brasserie.");
+  initializeMap("map-target");
   try {
     const fetchedBreweries = await getAll();
     const brewery = getRandomBrewery(fetchedBreweries);
@@ -29,35 +31,38 @@ $(document).ready(async function () {
       //   <p><a href='${brewery.website_url}' >Site</a></p>
       //   `;
       $breweryDiv.html(breweryCard.render());
+      setBreweryMarker(brewery);
+      // affichage des details brewery
+      displayBreweryDetails(brewery);
     } else {
-      $breweryDiv.text('Aucune brasserie trouvée...');
+      $breweryDiv.text("Aucune brasserie trouvée...");
     }
   } catch (error) {
-    console.log('Failed retrieving brewery: ', error);
-    $breweryDiv.text('Erreur dans la récupération des données.');
+    console.log("Failed retrieving brewery: ", error);
+    $breweryDiv.text("Erreur dans la récupération des données.");
   }
 
   try {
     const fetchedFiltered = await getFiltered(
-      'breweries',
-      'San Diego',
-      'California',
+      "breweries",
+      "San Diego",
+      "California"
     );
     console.log(fetchedFiltered);
   } catch (error) {
-    console.log('Failed retrieving brewery: ', error);
-    $breweryDiv.text('Erreur dans la récupération des données.');
+    console.log("Failed retrieving brewery: ", error);
+    $breweryDiv.text("Erreur dans la récupération des données.");
   }
 
   let statesList = [];
-  const $statesSelect = $('#states');
+  const $statesSelect = $("#states");
 
   try {
     const fetchedMetadata = await getMetadata();
     statesList = getAmericanStates(fetchedMetadata);
   } catch (error) {
-    console.log('Failed retrieving metas: ', error);
-    $breweryDiv.text('Erreur dans la récupération des metadonnées.');
+    console.log("Failed retrieving metas: ", error);
+    $breweryDiv.text("Erreur dans la récupération des metadonnées.");
   }
 
   console.log(statesList);
@@ -70,3 +75,45 @@ $(document).ready(async function () {
 
   // TODO: Search func, modular cleanup, logic for map render integration and raw UX
 });
+
+function displayBreweryDetails(brewery) {
+  // masquer la liste des resultats et afficher la zone de detail
+  $("#results-list").hide();
+
+  $("#brewery-details-area").show();
+
+  // mettre a jour les informations de la barsserie
+  $("#detail-name").text(brewery.name);
+  $("#detail-type").text(brewery.brewery_type);
+
+  // format addresse complet
+  const address = [
+    brewery.street,
+    `${brewery.city}, ${brewery.state_province}`,
+    brewery.country,
+    brewery.postal_code,
+  ]
+    .filter((line) => line)
+    .join("<br>");
+  $("#detail-address").html(address);
+
+  // telephone
+  if (brewery.phone) {
+    $("#detail-phone").text(`Tel: ${brewery.phone}`).parent().show();
+  } else {
+    $("#detail-phone").parent().hide();
+  }
+
+  // site web
+  const websiteURL = brewery.website_url;
+  const $websiteLink = $("#detail-website");
+  if (websiteURL) {
+    $websiteLink.attr("href", websiteURL);
+    $websiteLink.text("Visiter le site: ");
+    $websiteLink.show();
+  } else {
+    $websiteLink.hide();
+  }
+
+  setBreweryMarker(brewery);
+}
