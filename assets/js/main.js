f3dcd2cb9238401ca9250b58d36c1605727e1788
Sylvain Chambon <chambon.sylvain@gmail.com>
2025-11-01 10:47:16 +0100

    fix: errors due to merge confilcts...

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,14 @@ import {
   getRandom,
   getFiltered,
   getMetadata,
-} from "./services/openBreweryService.js";
-import {
-  getRandomBreweryFromList,
-  getAmericanStates,
-} from "./utils/openBreweryUtils.js";
-import { BreweryCardBuilder } from "./builders/builders.js";
-import { initializeMap, setBreweryMarker } from "./services/map.js";
-  getAmericanTypes,
-  toTitleCase,
-} from './utils/openBreweryUtils.js';
+} from './services/openBreweryService.js';
+import { getAmericanStates, toTitleCase } from './utils/openBreweryUtils.js';
+import { initializeMap, setBreweryMarker } from './services/map.js';
 import { BreweryCardBuilder } from './builders/builders.js';
 $(document).ready(async function () {
-  const $breweryDiv = $("#random-brewery");
+  const $breweryDiv = $('#random-brewery');
   $breweryDiv.text("Récupération d'une brasserie.");
-  initializeMap("map-target");
+  initializeMap('map-target');
   try {
     // const fetchedBreweries = await getAll();
     const breweryRequest = await getRandom();
@@ -36,11 +29,11 @@ $(document).ready(async function () {
       // affichage des details brewery
       displayBreweryDetails(brewery);
     } else {
-      $breweryDiv.text("Aucune brasserie trouvée...");
+      $breweryDiv.text('Aucune brasserie trouvée...');
     }
   } catch (error) {
-    console.log("Failed retrieving brewery: ", error);
-    $breweryDiv.text("Erreur dans la récupération des données.");
+    console.log('Failed retrieving brewery: ', error);
+    $breweryDiv.text('Erreur dans la récupération des données.');
   }
 
   let fetchedFiltered = [];
@@ -67,12 +60,12 @@ $(document).ready(async function () {
       });
     }
   } catch (error) {
-    console.log("Failed retrieving brewery: ", error);
-    $breweryDiv.text("Erreur dans la récupération des données.");
+    console.log('Failed retrieving brewery: ', error);
+    $breweryDiv.text('Erreur dans la récupération des données.');
   }
 
   let statesList = [];
-  const $statesSelect = $("#states");
+  const $statesSelect = $('#states');
 
   let typesList = [];
   const $typeSelect = $('#type');
@@ -80,10 +73,10 @@ $(document).ready(async function () {
   try {
     const fetchedMetadata = await getMetadata();
     statesList = getAmericanStates(fetchedMetadata);
-    typesList = getAmericanTypes(fetchedMetadata)
+    typesList = getAmericanTypes(fetchedMetadata);
   } catch (error) {
-    console.log("Failed retrieving metas: ", error);
-    $breweryDiv.text("Erreur dans la récupération des metadonnées.");
+    console.log('Failed retrieving metas: ', error);
+    $breweryDiv.text('Erreur dans la récupération des metadonnées.');
   }
 
   console.log(statesList);
@@ -101,20 +94,18 @@ $(document).ready(async function () {
     });
   }
 
-
-
   // TODO: Search func, modular cleanup, logic for map render integration and raw UX
 });
 
 function displayBreweryDetails(brewery) {
   // masquer la liste des resultats et afficher la zone de detail
-  $("#results-list").hide();
+  $('#results-list').hide();
 
-  $("#brewery-details-area").show();
+  $('#brewery-details-area').show();
 
   // mettre a jour les informations de la barsserie
-  $("#detail-name").text(brewery.name);
-  $("#detail-type").text(brewery.brewery_type);
+  $('#detail-name').text(brewery.name);
+  $('#detail-type').text(brewery.brewery_type);
 
   // format addresse complet
   const address = [
@@ -124,22 +115,22 @@ function displayBreweryDetails(brewery) {
     brewery.postal_code,
   ]
     .filter((line) => line)
-    .join("<br>");
-  $("#detail-address").html(address);
+    .join('<br>');
+  $('#detail-address').html(address);
 
   // telephone
   if (brewery.phone) {
-    $("#detail-phone").text(`Tel: ${brewery.phone}`).parent().show();
+    $('#detail-phone').text(`Tel: ${brewery.phone}`).parent().show();
   } else {
-    $("#detail-phone").parent().hide();
+    $('#detail-phone').parent().hide();
   }
 
   // site web
   const websiteURL = brewery.website_url;
-  const $websiteLink = $("#detail-website");
+  const $websiteLink = $('#detail-website');
   if (websiteURL) {
-    $websiteLink.attr("href", websiteURL);
-    $websiteLink.text("Visiter le site: ");
+    $websiteLink.attr('href', websiteURL);
+    $websiteLink.text('Visiter le site: ');
     $websiteLink.show();
   } else {
     $websiteLink.hide();
